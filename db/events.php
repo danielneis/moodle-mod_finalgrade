43f3b16bb51b464f4fb1d41b58f032f156182edb
Daniel Neis Araujo <daniel@setic.ufsc.br>
2013-07-16 15:40:03 -0300

    Firt version, minor changes from template

---
+++ db/events.php
@@ -0,0 +1,10 @@
+<?php
+
+$handlers = array (
+    'course_final_grade_regraded' => array (
+        'handlerfile'      => '/mod/finalgrade/lib.php',
+        'handlerfunction'  => 'finalgrade_course_final_grade_regraded',
+        'schedule'         => 'instant',
+        'internal'         => 1,
+    ),
+);

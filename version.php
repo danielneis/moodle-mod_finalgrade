4149d08032ba09a3383a3aabc44069583eaa9c4e
Daniel Neis Araujo <danielneis@gmail.com>
2018-05-24 18:36:22 -0300

    bump version

--- version.php
+++ version.php
@@ -28,8 +28,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018032803;      // The current module version (Date: YYYYMMDDXX)
-$plugin->requires  = 2017111302;      // Requires this Moodle version
-$plugin->release = 1;
+$plugin->version   = 2018052400;
+$plugin->requires  = 2017111302;
+$plugin->release = 2;
 $plugin->maturity   = MATURITY_STABLE;
-$plugin->component = 'mod_finalgrade'; // To check on upgrade, that module sits in correct place
+$plugin->component = 'mod_finalgrade';
